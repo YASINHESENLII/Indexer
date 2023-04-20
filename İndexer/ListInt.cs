950f801c0adf59a3c28bd6d2c234979310fe129f
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-20 06:02:54 +0400

    Indexer

---
+++ İndexer/ListInt.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İndexer
+{
+    internal class ListInt
+    {
+        private int[] arr;
+
+        public int this[int index] {
+            get { 
+                if (index < arr.Length) 
+                { return arr[index]; }
+                else
+                {
+                    return arr[arr.Length - 1];
+                }
+            } 
+            set { arr[index] = value;  } }
+
+        public int Length { get { return arr.Length; } }
+        public ListInt()
+        {
+            arr = new int[0];
+        }
+        public ListInt(int length)
+        {
+            arr = new int[length];
+        }
+        public ListInt(params int[] nums)
+        {
+            arr = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] = nums[i];
+            }
+        }
+        
+        public void Add( int value)
+        {
+            
+                
+
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length -1] = value;
+            
+
+        }
+       public void AddRange(params int[] nums)
+        {
+            int news = arr.Length;
+            Array.Resize(ref arr, arr.Length + nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                
+
+                arr[news + i] = nums[i];
+
+            }
+
+        }
+        public bool Contains(int num)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {if (arr[i] == num)
+                
+                return true;
+            }
+            
+            return false;   
+        }
+
+        public void Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                
+            }
+            Console.WriteLine(sum);
+        }
+        public override string ToString()
+        {
+            string a = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a += arr[i];
+                
+                if (i < arr.Length - 1)
+                {
+                    a += ",";
+                }
+            }
+            return a;
+        }
+        public void Remove(int num)
+        {
+            int[] newArray = new int[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==num)
+                {
+                    Array.Resize(ref newArray, newArray.Length +1);
+                    newArray[newArray.Length-1] = i;
+
+                }
+                
+            }
+            newArray = arr;
+            
+
+        }
+        public void RemoveRange(params int[] nums)
+        {
+
+        }
+
+        
+        
+
+
+    }
+}
---
+++ İndexer/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace İndexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ListInt num = new ListInt(2, 3, 4);
+
+
+            num.Add(1);
+            num.AddRange(1, 6);
+
+            Console.WriteLine(num.ToString());
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+            Console.WriteLine(num.Contains(1));
+
+            num.Sum();
+
+
+
+            num.Remove(3);
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+
+        }
+    }
+    
+
+    
+}
